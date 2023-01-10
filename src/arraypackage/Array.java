c913198d3e59869ab153571fd3174c5cfd5b56ca
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-09 21:42:08 -0800

    count characters in word

--- src/arraypackage/Array.java
+++ src/arraypackage/Array.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Array {
 
 	public static void main(String[] args) {
+
 		int number[] = { 1, 2, 22, 2 };
 		int largest = 0;
 		for (int i = 0; i < number.length; i++) {
@@ -15,16 +16,23 @@ public class Array {
 				largest = number[i];
 			}
 		}
+		System.out.println("**********************");
 		System.out.println("Qustion 1");
 		System.out.println("Finding largest number using 1 for loop");
-
 		System.out.println("Largest number: " + largest);
-		
-		System.out.println("**********************");
-		System.out.println("Qustion 2");
-		///////////
 
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("**********************");
+		System.out.println("Qustion 2");
+
+		System.out.println("Enter a word: ");
+		String word1 = scanner.next();
+
+		System.out.println("The word '" + word1 + "' has " + word1.length() + " characters.");
+
+		System.out.println("**********************");
+		System.out.println("Qustion 3");
 
 		while (true) {
 			System.out.print("Enter a  first number : ");
@@ -41,10 +49,10 @@ public class Array {
 				System.out.println("The number is out of range. Please try again.");
 			}
 		}
-		
+
 		System.out.println("**********************");
-		System.out.println("Qustion 3");
-		
+		System.out.println("Qustion 4");
+
 		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5 };
 		System.out.println("Enter number: ");
 		int num = scanner.nextInt();
@@ -55,67 +63,67 @@ public class Array {
 			}
 		}
 		System.out.println(num + " appears " + count + " times in the array");
-		
+
 		System.out.println("**********************");
-		System.out.println("Qustion 4");
-		
-		int[] numbers3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		System.out.println("Qustion 5");
+
+		int[] numbers3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		System.out.println("Enter number: ");
-		int num3= scanner.nextInt();
+		int num3 = scanner.nextInt();
 		List<int[]> pairs = new ArrayList<>();
 		for (int i = 0; i < numbers3.length; i++) {
-		  for (int j = i + 1; j < numbers3.length; j++) {
-		    if (numbers3[i] + numbers3[j] == num3) {
-		      pairs.add(new int[]{numbers3[i], numbers3[j]});
-		    }
-		  }
+			for (int j = i + 1; j < numbers3.length; j++) {
+				if (numbers3[i] + numbers3[j] == num3) {
+					pairs.add(new int[] { numbers3[i], numbers3[j] });
+				}
+			}
 		}
 
 		for (int[] pair : pairs) {
-		  System.out.println("["+pair[0] + "," + pair[1]+"]");
+			System.out.println("[" + pair[0] + "," + pair[1] + "]");
 		}
-		
+
 		System.out.println("**********************");
-		System.out.println("Qustion 5");
-		
-		//int[] array = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-		int[] array = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4};
+		System.out.println("Qustion 6");
+
+		int[] array = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
 
 		int[] result = new int[array.length];
 		int index = 0;
 		for (int i = 0; i < array.length; i++) {
-		    boolean isDuplicate = false;
-		    for (int j = 0; j < i; j++) {
-		        if (array[i] == array[j]) {
-		            isDuplicate = true; 
-		            break;
-		        }
-		        
-		    }
-		    if (!isDuplicate) {
-		        result[index++] = array[i];
-		        
-		    }
-		    
+			boolean isDuplicate = false;
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j]) {
+					isDuplicate = true;
+					break;
+				}
+
+			}
+			if (!isDuplicate) {
+				result[index++] = array[i];
+
+			}
+
 		}
-		//System.out.println("Input "+ "[1, 1, 2, 2, 3, 3, 4, 4, 5, 5]");
+
 		result = Arrays.copyOf(result, index);
-		
-		System.out.println("Length of new array: "+result.length + "\n" +" Output "+ Arrays.toString(result));
-		
-		scanner.close();
+
+		System.out.println("Length of new array: " + result.length + "\n" + " Output " + Arrays.toString(result));
+
 		System.out.println("**********************");
-		System.out.println("Qustion 6");
-		
-		int[] firstArray = {1, 2, 3, 4, 5};        //source array  
-		int[] secondArray = {6, 7, 8, 9, 10};  //destination array  
-		int fal = firstArray.length;        //determines length of firstArray  
-		int sal = secondArray.length;   //determines length of secondArray  
-		int[] resultt = new int[fal + sal];  //resultant array of size first array and second array  
-		System.arraycopy(firstArray, 0, resultt, 0, fal);  
-		System.arraycopy(secondArray, 0, resultt, fal, sal);  
+		System.out.println("Qustion 7");
+
+		int[] firstArray = { 1, 2, 3, 4, 5 }; // source array
+		int[] secondArray = { 6, 7, 8, 9, 10 }; // destination array
+		int fal = firstArray.length; // determines length of firstArray
+		int sal = secondArray.length; // determines length of secondArray
+		int[] resultt = new int[fal + sal]; // resultant array of size first array and second array
+		System.arraycopy(firstArray, 0, resultt, 0, fal);
+		System.arraycopy(secondArray, 0, resultt, fal, sal);
 		System.out.println(Arrays.toString(resultt));
-		
+
+		scanner.close();
+
 	}
 
 }
