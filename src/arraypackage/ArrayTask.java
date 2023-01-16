ac233cee20c7e2791a17046df11dac1442b3ee16
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-15 23:04:40 -0800

    third highest number

--- src/arraypackage/ArrayTask.java
+++ src/arraypackage/ArrayTask.java
@@ -7,8 +7,9 @@ public class ArrayTask {
 	public static void main(String[] args)
 
 	{
-		System.out.println("**********************");
 		System.out.println("Finging the third highest number");
+		System.out.println("*********************");
+
 		int array[] = { 1, 5, 9, 3, 7, 8, 2, 4, 6 };
 		Arrays.sort(array);
 
